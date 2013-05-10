6cd1a34e074b377681a55d3b1bd5043889f97158
Jared Banyard <JaredBanyard@gmail.com>
2013-05-10 15:16:38 -0400

    More tweaking and progress.. working api token exchange.  Bad formed requests though.

--- src/com/tachyondev/android/glass/looking/MainActivity.java
+++ src/com/tachyondev/android/glass/looking/MainActivity.java
@@ -27,6 +27,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
@@ -45,7 +46,14 @@ import android.view.WindowManager.LayoutParams;
 import android.widget.TextView;
 
 import com.google.api.client.googleapis.extensions.android.accounts.GoogleAccountManager;
+import com.google.api.client.util.DateTime;
 import com.google.api.services.youtube.YouTubeScopes;
+import com.google.api.services.youtube.model.LiveBroadcast;
+import com.google.api.services.youtube.model.LiveBroadcastSnippet;
+import com.google.api.services.youtube.model.LiveBroadcastStatus;
+import com.google.api.services.youtube.model.LiveStream;
+import com.google.api.services.youtube.model.LiveStreamCdn;
+import com.google.api.services.youtube.model.LiveStreamSnippet;
 
 public class MainActivity extends Activity {
 
@@ -60,7 +68,7 @@ public class MainActivity extends Activity {
 
     private Handler mHandler = new Handler();
 
-    private static String scope = YouTubeScopes.YOUTUBE + " " + YouTubeScopes.YOUTUBE_READONLY;
+    private static String scope = YouTubeScopes.YOUTUBE;
 
     private static String email;
 
@@ -72,6 +80,11 @@ public class MainActivity extends Activity {
     private static String access_token;
     private static String token_type;
     private static String refresh_token;
+
+    private LiveBroadcast broadcast;
+    private LiveStream stream;
+
+//    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,7 +101,14 @@ public class MainActivity extends Activity {
 
         getWindow().addFlags(LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        new AuthTask().execute();
+//        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+//        access_token = prefs.getString("Token", null);
+
+        if (access_token == null) {
+            new AuthTask().execute();
+        } else {
+            insert();
+        }
     }
 
     @Override
@@ -105,14 +125,12 @@ public class MainActivity extends Activity {
             HttpPost httppost = new HttpPost("https://accounts.google.com/o/oauth2/device/code");
 
             try {
-                // Add your data
                 List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
                 nameValuePairs.add(new BasicNameValuePair("client_id", CLIENT_ID));
                 nameValuePairs.add(new BasicNameValuePair("scope", scope));
 
                 httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-                // Execute HTTP Post Request
                 HttpResponse response = httpclient.execute(httppost);
 
                 Log.e(TAG, "Status: " + response.getStatusLine());
@@ -185,7 +203,6 @@ public class MainActivity extends Activity {
             HttpPost httppost = new HttpPost("https://accounts.google.com/o/oauth2/token");
 
             try {
-                // Add your data
                 List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
                 nameValuePairs.add(new BasicNameValuePair("client_id", CLIENT_ID));
                 nameValuePairs.add(new BasicNameValuePair("client_secret", CLIENT_SECRET));
@@ -194,7 +211,6 @@ public class MainActivity extends Activity {
 
                 httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-                // Execute HTTP Post Request
                 HttpResponse response = httpclient.execute(httppost);
 
                 Log.e(TAG, "Status: " + response.getStatusLine());
@@ -227,6 +243,10 @@ public class MainActivity extends Activity {
                     refresh_token = json.getString("refresh_token");
                     expires_in = json.getInt("expires_in");
                     displayAuth(2);
+//                    Editor editor = prefs.edit();
+//                    editor.putString("Token", access_token);
+//                    editor.commit();
+                    insert();
                 } catch (JSONException e) {
                     expires_in -= interval;
                     displayAuth(1);
@@ -243,27 +263,29 @@ public class MainActivity extends Activity {
             new VerifyTask().execute();
         }
     };
-    
 
-    public class InsertTask extends AsyncTask<String, Void, JSONObject>
+    public class BroadcastInsertTask extends AsyncTask<Void, Void, JSONObject>
     {
 
         @Override
-        protected JSONObject doInBackground(String... params) {
+        protected JSONObject doInBackground(Void... params) {
             HttpClient httpclient = new DefaultHttpClient();
             HttpPost httppost = new HttpPost("https://www.googleapis.com/youtube/v3/liveBroadcasts");
-
             try {
-                // Add your data
-                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-                nameValuePairs.add(new BasicNameValuePair("client_id", CLIENT_ID));
-                nameValuePairs.add(new BasicNameValuePair("client_secret", CLIENT_SECRET));
-                nameValuePairs.add(new BasicNameValuePair("code", device_code));
-                nameValuePairs.add(new BasicNameValuePair("grant_type", GRANT_TYPE));
+                httppost.setHeader("Authorization", "Bearer " + access_token);
+//                BasicHttpParams httpParams = new BasicHttpParams();
+//                broadcast.getpa
+//                httppost.setParams(httpParams);
 
-                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+//                nameValuePairs.add(new BasicNameValuePair("part", broadcast.toString()));
+//                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-                // Execute HTTP Post Request
+                Log.e(TAG, broadcast.toString());
+                StringEntity se = new StringEntity(broadcast.toString());
+
+                httppost.setEntity(se);
+
                 HttpResponse response = httpclient.execute(httppost);
 
                 Log.e(TAG, "Status: " + response.getStatusLine());
@@ -273,14 +295,10 @@ public class MainActivity extends Activity {
                 for (String line = null; (line = reader.readLine()) != null;) {
                     builder.append(line).append("\n");
                 }
-                JSONTokener tokener = new JSONTokener(builder.toString());
-                JSONObject json = new JSONObject(tokener);
-                return json;
+                Log.e(TAG, builder.toString());
             } catch (ClientProtocolException e) {
                 e.printStackTrace();
             } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
                 e.printStackTrace();
             }
             return null;
@@ -290,19 +308,40 @@ public class MainActivity extends Activity {
         protected void onPostExecute(JSONObject json) {
             if (json != null) {
                 Log.e(TAG, json.toString());
-                try {
-                    access_token = json.getString("access_token");
-                    token_type = json.getString("token_type");
-                    refresh_token = json.getString("refresh_token");
-                    expires_in = json.getInt("expires_in");
-                    displayAuth(2);
-                } catch (JSONException e) {
-                    expires_in -= interval;
-                    displayAuth(1);
-                    mHandler.postDelayed(VerifyRunnable, interval * 1000);
-                }
             }
         }
     }
 
+    public void insert() {
+        try {
+            LiveBroadcastSnippet broadcastSnippet = new LiveBroadcastSnippet();
+            broadcastSnippet.setTitle("Test Broadcast");
+            broadcastSnippet.setScheduledStartTime(new DateTime("2024-01-30T00:00:00.000Z"));
+            broadcastSnippet.setScheduledEndTime(new DateTime("2024-01-31T00:00:00.000Z"));
+
+            LiveBroadcastStatus status = new LiveBroadcastStatus();
+            status.setPrivacyStatus("private");
+
+            broadcast = new LiveBroadcast();
+            broadcast.setKind("youtube#liveBroadcast");
+            broadcast.setSnippet(broadcastSnippet);
+            broadcast.setStatus(status);
+            Log.e(TAG, broadcast.toString());
+
+            LiveStreamSnippet streamSnippet = new LiveStreamSnippet();
+            streamSnippet.setTitle("Test Stream");
+
+            LiveStreamCdn cdn = new LiveStreamCdn();
+            cdn.setFormat("360p");
+            cdn.setIngestionType("rtmp");
+
+            stream = new LiveStream();
+            stream.setKind("youtube#liveStream");
+            stream.setSnippet(streamSnippet);
+            stream.setCdn(cdn);
+            new BroadcastInsertTask().execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
